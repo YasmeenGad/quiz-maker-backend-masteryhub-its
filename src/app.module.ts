b11c29cf712a7d724ab9fd1d1915f6442847f2cb
YasmeenGad <gadyasmeen335@gmail.com>
2025-09-15 16:37:52 +0300

    chore: add main bootstrap with ValidationPipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,37 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
-import { QuizzesModule } from './quizzes/quizzes.module';
-import { QuestionsModule } from './questions/questions.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, AuthModule, QuizzesModule, QuestionsModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-
-@Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',   
-      port: 3306,
-      username: 'root',  
-      password: '1234', 
-      database: 'quiz_maker',   
-      entities: [ ],
-      synchronize: true, 
-      // add
-    }),
-     QuizzesModule, // add
-      UsersModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
-})
-
-
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import { ValidationPipe } from '@nestjs/common';
+
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  await app.listen(process.env.PORT || 3000);
+  console.log(`Server started on ${process.env.PORT || 3000}`);
 }
 bootstrap();
