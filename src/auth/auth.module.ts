ff51acdedca15095268f676496dd373af663a34b
YasmeenGad <gadyasmeen335@gmail.com>
2025-09-15 17:55:51 +0300

    refactor: refactor auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,8 +4,9 @@ import { PassportModule } from '@nestjs/passport';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { JwtStrategy } from '../auth/jwt.strategy';
+import { JwtStrategy } from './jwt.strategy';
 import { UsersModule } from '../users/users.module';
+import { BlacklistService } from './blacklist.service';
 
 @Module({
   imports: [
@@ -21,7 +22,7 @@ import { UsersModule } from '../users/users.module';
       inject: [ConfigService],
     }),
   ],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService, JwtStrategy, BlacklistService],
   controllers: [AuthController],
   exports: [AuthService],
 })
