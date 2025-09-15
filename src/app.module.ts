c0486be5cc42508108e3f7e86c0a2c1751d5be25
YasmeenGad <gadyasmeen335@gmail.com>
2025-09-15 16:35:32 +0300

    chore: add .env.example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { QuizzesModule } from './quizzes/quizzes.module';
+import { QuestionsModule } from './questions/questions.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [UsersModule, AuthModule, QuizzesModule, QuestionsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
 
 @Module({
   imports: [
@@ -14,9 +24,14 @@ import { AppService } from './app.service';
       database: 'quiz_maker',   
       entities: [ ],
       synchronize: true, 
+      // add
     }),
+     QuizzesModule, // add
+      UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
+
+
 export class AppModule {}
